168e64763d651f96e3dcdfe05e46c07f24c3fa65
Aras Valizadeh <arasvalizadeh@gmail.com>
2024-08-11 12:40:55 +0330

    python part of poisson done

--- HyperGeometric-Distribuation/Python/probability.py
+++ HyperGeometric-Distribuation/Python/probability.py
@@ -6,39 +6,48 @@ def hypergeometric_pmf(k, N, K, n):
     denominator = comb(N, n)
     return numerator / denominator
 
-def hypergeometric_cdf(k, N, K, n):
-    cumulative_prob = 0
-    for i in range(k, n + 1):
-        cumulative_prob += hypergeometric_pmf(i, N, K, n)
-    return cumulative_prob
+def expected_value(x,y):
+    sum = 0
+    for i in range(len(y)):
+        sum += x[i] * y[i]
+    return sum 
+
+def expected_value2(x,y):
+    sum = 0
+    for i in range(len(y)):
+        sum += x[i]**2 * y[i]
+    return sum
 
 N = 10000  # Total number of devices
 K = 200    # Total number of infected devices
 n = 500    # Number of devices selected for inspection
-k = 10     # Number of infected devices we want to detect
 
 x = []
 y = []
 
-for i in range (200):
+for i in range(200):
     x.append(i)
     y.append(hypergeometric_pmf(i, N, K, n))
 
-y1 = [y[0]]
+print("expected value is :",expected_value(x,y))
+print("variance is : ",expected_value2(x,y) - (expected_value(x,y) ** 2))
 
-for i in range(1,200):
-    y1.append(y[i]+y1[i-1])
+y1 = [y[0]]
+for i in range(1, 200):
+    y1.append(y[i] + y1[i - 1])
 
 plt.figure(figsize=(10, 6))
 plt.plot(x, y, marker='.', linestyle='-', color='b')
-plt.xlabel('Number of infected device in sampling')
-plt.ylabel('Probability')
+plt.xlabel('Number of infected devices in sample')
+plt.ylabel('Probability (PMF)')
 plt.grid(True)
+plt.title('Hypergeometric PMF: Exactly k Infected Devices')
 plt.show()
 
 plt.figure(figsize=(10, 6))
-plt.plot(x, y1, marker='.', linestyle='-', color='b')
-plt.xlabel('Number of at least k infected device in sampling')
-plt.ylabel('Probability')
+plt.plot(x, y1, marker='.', linestyle='-', color='r')
+plt.xlabel('Number of infected devices in sample')
+plt.ylabel('Cumulative Probability (CDF)')
 plt.grid(True)
+plt.title('Hypergeometric CDF: Up to k Infected Devices')
 plt.show()
--- Poisson-Distribuation/Python/probability.py
+++ Poisson-Distribuation/Python/probability.py
@@ -1,1 +1,63 @@
-print('hello world!')
+import math
+import matplotlib.pyplot as plt
+
+def poisson_pmf(k, lambda_rate):
+    probability = (lambda_rate**k * math.exp(-lambda_rate)) / math.factorial(k)
+    return probability
+
+def expected_value(x,y):
+    sum = 0
+    for i in range(len(y)):
+        sum += x[i] * y[i]
+    return sum 
+
+def expected_value2(x,y):
+    sum = 0
+    for i in range(len(y)):
+        sum += x[i]**2 * y[i]
+    return sum
+
+lambda_rate = 30  
+x = []
+y = []
+for i in range (100):
+    x.append(i)
+    y.append(poisson_pmf(i, lambda_rate))
+
+print("expected value is :",expected_value(x,y))
+print("variance is : ",expected_value2(x,y) - (expected_value(x,y) ** 2))
+
+y1 = [y[0]]
+
+for j in range(1,len(y)):
+    y1.append(y1[j-1] + y[j])
+
+
+plt.figure(figsize=(10, 6))
+plt.plot(x, y, marker='*', linestyle='-', color='b')
+plt.xlabel('Number of calls')
+plt.ylabel('Probability of k calls happend in a min')
+plt.title('Poisson-Distribuation')
+plt.grid(True)
+plt.show()
+
+
+
+plt.figure(figsize=(10, 6))
+plt.plot(x, y1, marker='*', linestyle='-', color='g')
+plt.xlabel('Number of calls')
+plt.ylabel('Probability of up to k calls happend in a min')
+plt.title('Poisson-Distribuation')
+plt.grid(True)
+plt.show()
+
+
+prob_less_than_20 = sum(poisson_pmf(k, lambda_rate) for k in range(20))
+print(prob_less_than_20)
+
+
+prob_more_than_35 = 1 - sum(poisson_pmf(k, lambda_rate) for k in range(36))
+print(prob_more_than_35)
+
+prob_between_25_and_35 = sum(poisson_pmf(k, lambda_rate) for k in range(25, 36))
+print(prob_between_25_and_35)
